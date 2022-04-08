bbdd85004d60d6a1824377878961708c7acc7fea
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-04-08 15:17:34 +0900

    + 텍스트 추가 / 검색 기능 구현 + 리스트에서 색상 표시(RED) + 엔터로 이벤트 시행 + 재검색시 초기화 + 대소문자 구분없이 검색 허용

--- FullTextSearch/Form1.Designer.cs
+++ FullTextSearch/Form1.Designer.cs
@@ -29,11 +29,12 @@ namespace FullTextSearch
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -46,6 +47,23 @@ namespace FullTextSearch
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(408, 49);
             this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 23);
+            this.textBox1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -65,22 +83,6 @@ namespace FullTextSearch
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(14, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(299, 23);
-            this.textBox1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(319, 14);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -88,8 +90,9 @@ namespace FullTextSearch
             this.ClientSize = new System.Drawing.Size(432, 490);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FullTextSearch";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
--- FullTextSearch/Form1.cs
+++ FullTextSearch/Form1.cs
@@ -11,8 +11,18 @@ using System.Windows.Forms;
 
 namespace FullTextSearch
 {
+
+    /*
+        1. 버튼을 클릭하면 검색창 단어와 리스트 박스의 경로를 찾음 v
+        2. 리스트 박스의 경로를 통해서 파일 안에 데이터를 읽음 v
+        3. 파일 안에 데이터에 검색어가 포함되어 있으면 해당 리스트에 색깔 표시
+     */
+
+
     public partial class Form1 : Form
     {
+        string[] files = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +30,9 @@ namespace FullTextSearch
             listBox1.AllowDrop = true;
             listBox1.DragDrop += listBoxFiles_DragDrop;
             listBox1.DragEnter += listBoxFiles_DragEnter;
-
+            listBox1.DrawMode = DrawMode.OwnerDrawFixed;
+            listBox1.DrawItem += new DrawItemEventHandler(listBox1_DrawItem);
+            textBox1.KeyDown += textBox1_KeyDown;
         }
 
         private void listBoxFiles_DragEnter(object sender, DragEventArgs e)
@@ -33,22 +45,88 @@ namespace FullTextSearch
         private void listBoxFiles_DragDrop(object sender, DragEventArgs e)
         {
             Debug.WriteLine("listBoxFiles_DragDrop");
-            // filename add
-            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            files = (string[])e.Data.GetData(DataFormats.FileDrop);
             foreach (string file in files)
             {
                 listBox1.Items.Add(file);
-                // file line read
-                string textValue = System.IO.File.ReadAllText(file);
-                Debug.WriteLine(textValue);
-
             }
-
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectionMode = SelectionMode.MultiExtended; // 다중 선택 모드
+            string result = "";
+            string word = textBox1.Text;
+            List<int> indexList = new List<int>();
+
+            // selected 초기화
+            listBox1.SelectedIndex = -1;
+
+            foreach (var input_items in listBox1.Items) 
+            {
+                // 파일 경로 담기
+                result += string.Format("{0} ", input_items);
+                string textValue = System.IO.File.ReadAllText(input_items.ToString());
+                Debug.WriteLine(textValue);
+                // 해당하는 인덱스 담기
+                if (textValue.Contains(word, StringComparison.OrdinalIgnoreCase))
+                {
+                    Debug.WriteLine("글자가 포함되어 있습니다.");
+                    indexList.Add(listBox1.Items.IndexOf(input_items));
+                }
+
+            }
+            Debug.WriteLine(indexList);
+            //해당하는 데이터 selected 하기
+            foreach (var index in indexList)
+            {
+                listBox1.SetSelected(index, true);
+            }
+
+            Debug.WriteLine("button1_Click(list): "+ result);
+            Debug.WriteLine("button1_Click(word): " + word);
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+
+            bool isItemSelected = ((e.State & DrawItemState.Selected) == DrawItemState.Selected);
+            int itemIndex = e.Index;
+            if (itemIndex >= 0 && itemIndex < listBox1.Items.Count)
+            {
+                Graphics g = e.Graphics;
+
+                // Background Color
+                SolidBrush backgroundColorBrush = new SolidBrush((isItemSelected) ? Color.Red : Color.White);
+                g.FillRectangle(backgroundColorBrush, e.Bounds);
+
+                // Set text color
+                string itemText = listBox1.Items[itemIndex].ToString();
+
+                SolidBrush itemTextColorBrush = (isItemSelected) ? new SolidBrush(Color.White) : new SolidBrush(Color.Black);
+                g.DrawString(itemText, e.Font, itemTextColorBrush, listBox1.GetItemRectangle(itemIndex).Location);
+
+                // Clean up
+                backgroundColorBrush.Dispose();
+                itemTextColorBrush.Dispose();
+            }
+
+            e.DrawFocusRectangle();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+        }
+
     }
 }
